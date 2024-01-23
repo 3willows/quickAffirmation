c6efb16faf1cdcced37cfb7330a1e8f51456f5e2
Jasper Wong <wlcjasper@gmail.com>
2024-01-23 12:50:21 +0800

    add link to video demo

--- src/components/PopUpContent.js
+++ src/components/PopUpContent.js
@@ -11,11 +11,18 @@ export const PopUpContent = () => {
   return (
     <div className="go-left">
       <Modal isOpen={isModalOpen} onClose={closeModal}>
-        <p><strong>Quick Affirmation</strong></p>
-        <p className="">
-          {" "}
-          Enter data once and it appears everywhere needed. 
+        <p>
+          <strong>Quick Affirmation</strong>
         </p>
+        <p className=""> Enter data once and it appears everywhere needed.</p>
+
+        <a
+          href="https://github.com/3willows/quickAffirmation/assets/111284156/3e0880f7-c3f4-4b46-ad4a-a8f001b296fd
+"
+          target="_blank" rel="noreferrer"
+        >
+          Video demo
+        </a>
       </Modal>
     </div>
   )
