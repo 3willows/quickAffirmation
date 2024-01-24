0db2bb1dc9f9d6899289feda7a8af651defae3e8
Jasper Wong <wlcjasper@gmail.com>
2024-01-24 23:47:16 +0800

    just keep the same link for mobile and desktop

--- src/components/PopUpContent.js
+++ src/components/PopUpContent.js
@@ -18,9 +18,10 @@ export const PopUpContent = () => {
           <strong>Quick Affirmation</strong>
         </p>
         <p className=""> Enter data once and it appears everywhere needed.</p>
+        {/* likely best to use the same link, users can rotate if they like */}
         {isMobile? 
                 <a
-                href="https://github.com/3willows/quickAffirmation/assets/111284156/1257b8af-6792-4155-951e-142d35f13611
+                href="https://github.com/3willows/quickAffirmation/assets/111284156/1dbe2678-772d-431d-ad68-d3f996e2e935
       "
                 target="_blank"
                 rel="noreferrer"
