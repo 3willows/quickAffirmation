df554513f198a823aa507f5fd870d86bb5b46d22
Jasper Wong <wlcjasper@gmail.com>
2024-01-26 21:46:22 +0800

    further case names for future extension

--- src/components/CaseNumberHeading.js
+++ src/components/CaseNumberHeading.js
@@ -19,6 +19,26 @@ const highCourtcaseTypes = [
   },
 ]
 
+// more
+
+const moreHcCaseTypes = [
+  { English: "Constitutional and Administrative Law Proceedings", Chinese: "憲法及行政訴訟"},
+  { English: "Admiralty Action", Chinese: "海事訢訟" },
+  { English: "Adoption Application", Chinese: "收養申請"  },
+  { English: "High Court Bankruptcy Interim Order", Chinese: "破產案臨時命令" },
+  { English: "Applicati on to set as這e a Statutory Demand", Chinese: "申請將法定要求償債書作廢" },
+  { English: "Construction and Arbitration Proceedings", Chinese: "建築及仲裁訴訟" },
+  { English: "Matrimonial Causes", Chinese: "婚姻訢訟", },
+  { English: "Confidential Miscellaneous Proceedings", Chinese: "機密雜項案件" },
+  { English: "Bookdebt Registration", Chinese: "帳面債項登記" },
+  { English: "Bill of Sale Registration", Chinese: "賣據登記" },
+  { English: "Stop Notice", Chinese: "停止通知書" },
+  { English: "Application for Discharge", Chinese: "釋放申請" },
+  { English: "Applications under the Mental Health Ordinance", Chinese: "根據《精神健康條例》提出的申請" },
+  { English: "Intellectual Property Case", Chinese: "知識產權案件" },
+  { English: "Reciprocal Enforcement Case", Chinese: "民商事相互執行案件" },
+]
+
 const districtCourtCaseTypes = [
   { Chinese: "民事訴訟案件", English: "CIVIL ACTION" },
   {
